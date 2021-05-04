ff84f13ab56f5ebfa5c8dcf439bc21e7726db4e3
Ajibade Olufemi <ajibadeolufemi94@gmail.com>
2021-05-04 11:49:07 +0100

    forgotten

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
+const App = () => {
+  return (
+    <ChatEngine 
+      height="100vh"
+      projectID="3e37d075-7840-4b0e-8a03-ed10f2b83a85"
+      userName="Femi"
+      userSecret="1234"
+      renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps} />}
+    />
+  );
+}
 
+//user: femi, secret: 1234
+//user: Motun, secret: motun
 
 export default App;
